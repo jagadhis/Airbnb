c459aedf68f22a0dbc5942e8712e19df2f2ae0fa
jagadhis <jwaran78@gmail.com>
2022-05-20 06:00:47 +0530

    added toolbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import bg from "../images/frontpagebg.png";
 import logo from "../images/airbnb.png";
-import { ConnectButton } from "web3uikit";
+import { useState } from "react";
+import { ConnectButton, Icon, Select, DatePicker, Input } from "web3uikit";
 const Home = () => {
   return (
     <>
@@ -25,13 +26,48 @@ const Home = () => {
       </div>
       <div className="tabContent">
         <div className="searchFields">
-          <div className="inputs">Location</div>
+          <div className="inputs">
+            Location
+            <Select
+              defaultOptionIndex={0}
+              onChange={(data) => console.log(data.label)}
+              options={[
+                { id: "NYC", label: "New York City" },
+                { id: "AUS", label: "Australia" },
+                { id: "MLD", label: "Maldives" },
+                { id: "BA", label: "Bali" },
+              ]}
+            />
+          </div>
           <div className="vl" />
-          <div className="inputs">Check In</div>
+          <div className="inputs">
+            Check In
+            <DatePicker
+              id="CheckIn"
+              onChange={(event) => console.log(event.date)}
+            />
+          </div>
           <div className="vl" />
-          <div className="inputs">Check Out</div>
+          <div className="inputs">
+            Check Out
+            <DatePicker
+              id="CheckOut"
+              onChange={(event) => console.log(event.date)}
+            />
+          </div>
           <div className="vl" />
-          <div className="inputs">Guests</div>
+          <div className="inputs">
+            Guests
+            <Input
+              value={2}
+              name="AddGuests"
+              type="number"
+              onChange={(event) => console.log(Number(event.target.value))}
+            />
+          </div>
+          <div className="searchButton">
+            <Icon fill="#ffffff" size={22} svg="search" />
+          </div>
         </div>
       </div>
     </>
